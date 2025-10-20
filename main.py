54440d4f45bb95280f3dc0b78622a8af4d8fceb7
Irastris <irastris15@gmail.com>
2025-10-19 21:17:06 -0400

    Hello world, beginnings of configuration

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 # Howl3D: Free & Open-Source 3D SBS Conversion
 __version__ = "0.0.1"
+
+# Global variables
+device = torch.device('cuda') # Set Torch device to be used for any applicable operations going forward
+thread_pool = ThreadPoolExecutor(max_workers=8) # Construct pool for potential multithreaded processes
+
+if __name__ == '__main__':
+    # Create global config to passing data down with
+    config = {}
+    # Add variables into the config
+    config["DEVICE"] = device
+    config["THREAD_POOL"] = thread_pool
+
+    print("Hello, world!")
