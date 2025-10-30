bc91a5d35d75e9b3b5ade06252685eb646791f29
Irastris <irastris15@gmail.com>
2025-10-30 17:58:35 -0400

    Finish implementing jobs

--- howl3d/job_manager.py
+++ howl3d/job_manager.py
@@ -1,6 +1,12 @@
 import hashlib
+import queue
+import threading
+import time
+import traceback
 from enum import Enum
 from pathlib import Path
+
+from howl3d.media_conversion import MediaConversion
 
 class JobStatus(Enum):
     Queued = 1
@@ -27,7 +33,46 @@ class Job:
 class JobManager:
     def __init__(self, concurrent_jobs=1):
         self.concurrent_jobs = concurrent_jobs
+        self.job_queue = queue.Queue()
+        self.active_jobs = {}
+        self.worker_thread = threading.Thread(target=self.process_jobs, daemon=True).start()
+        self.worker_thread_lock = threading.Lock()
 
     def submit_job(self, config, media_path):
         job = Job(config, media_path)
+        with self.worker_thread_lock:
+            self.job_queue.put(job)
+            self.active_jobs[job.id] = job
         print(f"Job submitted for {job.media_path} with a working directory of {job.config['working_dir']}")
+
+    @staticmethod
+    def return_exc(job, e):
+        return f"\n{traceback.format_exc()}" if job.config["enable_detailed_exceptions"] else e
+
+    def process_jobs(self):
+        while True:
+            try:
+                active_count = sum(1 for job in self.active_jobs.values() if job.status == JobStatus.Processing)
+                if active_count < self.concurrent_jobs:
+                    try:
+                        job = self.job_queue.get(timeout=1)
+                        job.status = JobStatus.Processing
+                        try:
+                            media_conversion = MediaConversion(job.config, job.media_path)
+                            media_conversion.process()
+                            job.status = JobStatus.Completed
+                            print(f"Job {job.id} completed!")
+                        except Exception as e:
+                            job.status = JobStatus.Failed
+                            print(f"Job {job.id} failed: {self.return_exc(job, e)}")
+                        finally:
+                            if job in self.job_queue.queue:
+                                self.job_queue.task_done()
+                    except queue.Empty:
+                        time.sleep(0.1)
+                        continue
+                else:
+                    time.sleep(0.1)
+            except Exception as e:
+                print(f"Error in worker thread: {e}")
+                time.sleep(1)
