e54b201a231708793f8ad128dbcd03289314fdc9
Irastris <irastris15@gmail.com>
2025-10-31 18:58:44 -0400

    Make heartbeat messages optional

--- howl3d/heartbeat.py
+++ howl3d/heartbeat.py
@@ -9,5 +9,6 @@ class Heartbeat:
     def __init__(self, job_id):
         self.job_id = job_id
 
-    def send(self, type, task, msg):
-        print(f"Job {self.job_id} sent heartbeat from task {task}: {msg}", flush=True)
+    def send(self, type, task, msg=None):
+        # If heartbeat contains a human-readable message, print it to the console
+        if msg: print(f"Job {self.job_id} sent heartbeat from task {task}: {msg}", flush=True)
